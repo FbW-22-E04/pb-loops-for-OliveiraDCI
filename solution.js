29509fb7563a5d5899c66d41e94ba69b520e8ce7
OliveiraDCI <oliveira.sein.dci@gmail.com>
2022-07-12 18:44:24 +0200

    Small adjustments, exercise concluded.

--- solution.js
+++ solution.js
@@ -1,3 +1,6 @@
+console.log("=========");
+console.log("=== 1 ===");
+console.log("=========");
 // 1
 let numArr = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
@@ -8,6 +11,9 @@ for (let i = 0; i < numArr.length; i++) {
 }
 console.log(sum);
 
+console.log("=========");
+console.log("=== 2 ===");
+console.log("=========");
 // 2
 let bottles = 0;
 let max = 5;
@@ -18,6 +24,9 @@ for (let i = 1; i <= max; i++) {
     : console.log(`There are ${i} bottles of beer on the table.`);
 }
 
+console.log("=========");
+console.log("=== 3 ===");
+console.log("=========");
 // 3
 let counterStart = 0;
 for (let i = 0; i <= 20; i++) {
@@ -25,22 +34,27 @@ for (let i = 0; i <= 20; i++) {
   i % 2 == 1 ? console.log(`${i} is odd`) : console.log(`${i} is even`);
 }
 
+console.log("=========");
+console.log("=== 4 ===");
+console.log("=========");
 // 4
 let start = 0;
-for (let i = 0; i <= 10; i++) {
+for (let i = 1; i <= 10; i++) {
   start += start[i];
 
-  console.log("---first part of question, current iteration times nine:");
-  console.log(`${i} * 9 = ${i * 9}`);
+  //   console.log("---first part of question, current iteration times nine:");
+  console.log(`${i} * 9 = ${i * 9}. Multiplication table of ${i}`);
 
-  console.log("---bonus part of question, nested loop:");
-  for (let rep = 0; rep <= 10; rep++) {
+  //   console.log("---bonus part of question, nested loop:");
+  for (let rep = 1; rep <= 10; rep++) {
     console.log(`${i} * ${rep} = ${i * rep}`);
   }
 }
 
+console.log("=========");
+console.log("=== 5 ===");
+console.log("=========");
 // 5
-
 for (let i = 1; i <= 100; i++) {
   if (i % 3 == 0 && i % 5 == 0) {
     console.log(i + " FizzBuzz");
@@ -53,6 +67,9 @@ for (let i = 1; i <= 100; i++) {
   }
 }
 
+console.log("=========");
+console.log("=== 6 ===");
+console.log("=========");
 // 6
 let totalSum = 0;
 for (let i = 1; i < 1000; i++) {
@@ -62,6 +79,9 @@ for (let i = 1; i < 1000; i++) {
 }
 console.log(totalSum);
 
+console.log("=========");
+console.log("=== 7 ===");
+console.log("=========");
 // 7 - BONUS
 for (let i = 1; i <= 1000; i++) {
   if (String(i).slice(-2) === "00") console.log(i);
@@ -79,14 +99,26 @@ for (let i = 9; i >= 0; i--) {
   console.log(i);
 }
 
-for (let i = 1; i <= 4; i++) {
-  console.log(i, i, i);
+let numList = "";
+for (let j = 1; j <= 4; j++) {
+  for (let i = 0; i < 3; i++) {
+    numList = numList + j + " ";
+    // console.log(i);
+  }
 }
-console.log("---");
-for (let i = 0; i <= 4; i++) {
-  console.log(String(i).concat(i).concat(i).concat(i));
-}
+console.log(numList);
 
+let numListTwo = "";
+for (let j = 0; j < 3; j++) {
+  for (let i = 0; i <= 4; i++) {
+    numListTwo = numListTwo + i + " ";
+  }
+}
+console.log(numListTwo);
+
+console.log("=========");
+console.log("=== 8 ===");
+console.log("=========");
 // 8
 let isPalindrome = "lol";
 let isPalindromeReverse = isPalindrome.split("").reverse().join("");
